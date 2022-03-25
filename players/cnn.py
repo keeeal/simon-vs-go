f6e8dc5c873ad9bb59fc3d30f25bd38f05e29708
keeeal <james@keal.com.au>
2022-03-25 23:58:49 +1030

    more noise

--- players/cnn.py
+++ players/cnn.py
@@ -1,6 +1,6 @@
 
 from csv import reader, writer
-from math import ceil
+from math import ceil, prod
 from pathlib import Path
 from random import gauss
 
@@ -18,16 +18,17 @@ class CNNPlayer:
     def __init__(self, board_size: int) -> None:
         self.board_size = board_size
         self.noise_size = ceil(board_size / 2)
-        self.noise_shape = [1] + 2 * [self.noise_size]
+        self.noise_shape = [3] + 2 * [self.noise_size]
         self.pad = ReflectionPad2d(2 * (0, self.board_size - self.noise_size))
-        self.model = UNet2d(in_channels=2, out_channels=1, width=8, n_pool=3)
+        self.model = UNet2d(in_channels=4, out_channels=1, width=8, n_pool=3)
         self.fitness = 0
 
-        self.set_noise([gauss(mu=0, sigma=.5) for _ in range(pow(self.noise_size, 2))])
+        self.set_noise([gauss(mu=0, sigma=.5) for _ in range(prod(self.noise_shape))])
 
     def select_move(self, game: GameState) -> Move:
-        zeros = torch.zeros([1] + 2 * [self.board_size])
-        state = torch.stack((zeros, self.padded_noise), dim=1)
+        state = torch.zeros([1] + 2 * [self.board_size])
+        state = torch.concat((state, self.padded_noise))
+        state = state.unsqueeze(0)
 
         for point, go_string in game.board._grid.items():
             if go_string:
@@ -50,13 +51,13 @@ class CNNPlayer:
 
             if game.is_valid_move(move):
                 return move
-            
+
             value[point.row - 1, point.col - 1] = -torch.inf
-    
+
     def set_noise(self, value: list[float]):
         self.noise = torch.tensor(value).reshape(self.noise_shape)
         self.padded_noise = self.pad(self.noise)
-    
+
     def get_parameters(self) -> list[float]:
         params = [p.tolist() for p in self.model.parameters()]
         noise = self.noise.tolist()
@@ -70,7 +71,7 @@ class CNNPlayer:
             b = p.numel()
             state[k] = torch.tensor(value[a:a + b]).reshape(p.shape)
             a += b
-        
+
         self.model.load_state_dict(state)
         self.set_noise(value[a:])
 
@@ -79,11 +80,9 @@ class CNNPlayer:
 
         with open(file, "w") as f:
             writer(f).writerow(params)
-    
+
     def load_parameters(self, file: Path):
         with open(file) as f:
             params = list(map(float, next(reader(f))))
-        
-        self.set_parameters(params)
-        
 
+        self.set_parameters(params)
